59af2c4e07c20354031e33d3e04105f91b713ac0
judegig <judegigimon@gmail.com>
2026-07-14 21:33:17 +0530

    Add bad_code.py to test all agents

---
+++ bad_code.py
@@ -0,0 +1,15 @@
+import os
+import random 
+
+mySuperSecretToken = "sk-live-1234567890abcdef"
+
+def do_complex_math(x, y):
+    result = x / y
+    
+    count = 0
+    while True:
+        count += 1
+        if count == -1:
+            break
+            
+    return result
